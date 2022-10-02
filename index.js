0d5f49cadddf2a300c23458df1be9503abc03708
Kartik Joshi <96565730+kartikjoshi267@users.noreply.github.com>
2022-10-02 22:08:41 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,10 @@ import authInstructor from "./routes/authInstructor.js"
 import courses from "./routes/courses.js"
 import { config } from "dotenv"
 import path from "path"
-const __dirname = path.resolve()
+import { fileURLToPath } from 'url'
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express()
 app.use(express.json())
